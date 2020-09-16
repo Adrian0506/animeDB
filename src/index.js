125ca94299b9f681d174b6f9a2a1a9e9d764c936
Adrian0506 <60869100+Adrian0506@users.noreply.github.com>
2020-09-16 00:03:55 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -7,20 +7,28 @@ document.querySelector('.form_data').addEventListener('submit', (e) => {
 
 
 
-
+ // Search function 
 async function getSearch() {
  let data = document.querySelector('._data').value
+ let alert = `<div class="alert alert-success animate"  role="alert">
+ Success search sent!
+ </div>`
+  document.querySelector('._results').innerHTML = ``
  console.log(data)
-  axios(`https://cors-anywhere.herokuapp.com/https://api.jikan.moe/v3/search/anime?q=${data}&page=1`).then(data => {
+  await axios(`https://cors-anywhere.herokuapp.com/https://api.jikan.moe/v3/search/anime?q=${data}&page=1`).then(data => {
    console.log(data)
-     sendInfo(data.data.results);
+   sendInfo(data.data.results);
+   document.querySelector('.alerts').insertAdjacentHTML('afterbegin', alert)
   }).catch(err => {
-      console.log(err) 
+    let badAlert = `<div class="alert alert-danger" role="alert">Bad request please try again.`
+    document.querySelector('.alerts').innerHTML = '';
+    document.querySelector('.alerts').insertAdjacentHTML('afterbegin', badAlert)  
+    console.log(err) 
     })
 
 }
 
-
+// Adds each element to the DOM
 function renderDom (data) {
 const html = `<div class="card" style="width: 15rem;">
 <img src="${data.image_url}" class="card-img-top" alt="...">
