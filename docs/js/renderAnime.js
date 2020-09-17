aa933b1e9301b31d4a58db19599c58a72b406bba
Adrian0506 <60869100+Adrian0506@users.noreply.github.com>
2020-09-17 08:46:35 -0700

    Add files via upload
    Error uploading docs folder (reupload)

--- docs/js/renderAnime.js
+++ docs/js/renderAnime.js
@@ -34,11 +34,30 @@ window.location = 'index.html'
 
 
 function renderAnime(info) {
+let genres = ''
+
+for (let i = 0; i < info.genres.length; i++) {
+  console.log(info.genres[i].name)
+genres += info.genres[i].name + ', '
+
+}
+
+
+
+
   let html = `
     <img src = ${info.image_url} class = 'image_resize'>
-    <h1 class = 'title_text'>${info.title}</h1>
+    <h1 class = 'title_text'><b>${info.title}</b></h1>
     <h1 class = 'rank_ani'><em>Rank: ${info.rank}</em></h1>
-    `
+    <h1 class = 'rank_score'><em>Score: ${info.score}/10</em></h1>
+    <h1 class = 'synp'>Synopsis:</h1>
+     <div class ='synp-block'>
+    <h1 class = 'anime_synp'>${info.synopsis}</h1>
+    </div>
+      <div class = 'anime_gen_container'>
+     <h1 class = 'anime_gen'>Genres: ${genres}</h1>
+      </div>
+     `
   document.querySelector('.white_border').insertAdjacentHTML('afterbegin', html)
 
 }
